f9dee7c6bd43ee71d069ee5ca7ae1ce737a013a1
Wkyouma <igorterplak297@gmail.com>
2024-12-16 17:50:49 -0300

    tag

---
+++ src/componentes/Galeria/Populares/index.jsx
@@ -0,0 +1,11 @@
+import TituloEstilizado from "../Titulo"
+
+const Populares = () => {
+    return (
+       <TituloEstilizado $alinhamento="center">
+        Populares
+       </TituloEstilizado>
+    )
+}
+
+export default Populares
--- src/componentes/Galeria/index.jsx
+++ src/componentes/Galeria/index.jsx
@@ -1,7 +1,29 @@
+import styled from 'styled-components'
+import Tags from './tags/index'
+import TituloEstilizado from './Titulo'
+import Populares from './Populares'
+
+const GaleriaContainer = styled.section`
+display:flex;`
+
+const Secao = styled.section`
+flex-grow:1;`
 
 
-const Galeria = () =>{
-    return(<h2>Navegue pela Galeria</h2>)
+
+const Galeria = () => {
+    return (
+        <>
+            
+            <Tags/>
+            <GaleriaContainer>
+                <Secao>
+                <TituloEstilizado>Navegue pela nossa galeria</TituloEstilizado>
+               
+                </Secao>
+                <Populares />
+            </GaleriaContainer>
+        </>)
 }
 
 export default Galeria
