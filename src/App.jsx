acbccf77847c5ad95a82947939977931865a2f45
Wkyouma <igorterplak297@gmail.com>
2024-12-16 23:16:12 -0300

    desteste

--- src/App.jsx
+++ src/App.jsx
@@ -39,7 +39,7 @@ function App() {
           <BarraLateral />
           <ConteudoGaleria>
             <Banner
-              texto="A galeria mais completa de fotos do espaço! 8=D"
+              texto="A galeria mais completa de fotos do espaço!"
               backgroundImage={bannerBackground}
             />
             <Galeria />
